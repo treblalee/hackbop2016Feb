f72cc7bfb22e33ee18acfb1789a5d35f47244527
Albert Lee <treblalee@ucla.edu>
2016-02-04 05:20:37 +0000

    add regression test for home page

--- regress.sh
+++ regress.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-ENDPOINT='http://ec2-54-152-49-181.compute-1.amazonaws.com:8000/similar?image=https://s3.amazonaws.com/treblalee.images/colym3002512010_p1_1-0._SH20_QL90_UY295_PIshopbop-db-sticker-2,BottomRight,1,1_UY295_.jpg'
-echo "Result of http get request for $ENDPOINT"
-curl -XGET $ENDPOINT
+HOST='http://ec2-54-152-49-181.compute-1.amazonaws.com'
+PORT='8000'
+HOME_ENDPOINT="$HOST:$PORT"
+SIM_ENDPOINT="$HOME_ENDPOINT/similar?image=https://s3.amazonaws.com/treblalee.images/colym3002512010_p1_1-0._SH20_QL90_UY295_PIshopbop-db-sticker-2,BottomRight,1,1_UY295_.jpg"
+echo "Result of http get request for $HOME_ENDPOINT"
+curl -XGET $HOME_ENDPOINT
+echo
+echo "Result of http get request for $SIM_ENDPOINT"
+curl -XGET $SIM_ENDPOINT
 echo

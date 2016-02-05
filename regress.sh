01a7a0162097da3c613aecc0eafe7949c6301cb5
Albert Lee <treblalee@ucla.edu>
2016-02-05 04:50:31 +0000

    change host to one provided by elastic ip

--- regress.sh
+++ regress.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-HOST='http://ec2-54-152-49-181.compute-1.amazonaws.com'
+HOST='http://ec2-52-23-125-147.compute-1.amazonaws.com'
 PORT='8000'
 HOME_ENDPOINT="$HOST:$PORT"
 SIM_ENDPOINT="$HOME_ENDPOINT/similar?image=https://s3.amazonaws.com/treblalee.images/colym3002512010_p1_1-0._SH20_QL90_UY295_PIshopbop-db-sticker-2,BottomRight,1,1_UY295_.jpg"
@@ -16,4 +16,5 @@ echo
 echo
 echo "Result of http get request for $SIM_PATH_ENDPOINT"
 curl -XGET $SIM_PATH_ENDPOINT
+echo
 echo

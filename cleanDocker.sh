3a9d32e6af930ba135bb7c65d459908a732d5453
Albert Lee <treblalee@ucla.edu>
2016-02-05 20:41:32 +0000

    delete all docker images in cleanup script

--- cleanDocker.sh
+++ cleanDocker.sh
@@ -4,5 +4,7 @@ docker kill $(docker ps -q)
 docker rm -v $(docker ps -a -q -f status=exited)
 # delete images
 docker rmi $(docker images -f "dangling=true" -q)
+# delete all images
+docker rmi $(docker images -q)
 # delete vfs
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes

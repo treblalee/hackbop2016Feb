76c955b140b5190fc03411429432a82a0c956093
Albert Lee <treblalee@ucla.edu>
2016-02-05 20:21:57 +0000

    add cleanDocker script

---
+++ cleanDocker.sh
@@ -0,0 +1,8 @@
+# kill running containers
+docker kill $(docker ps -q)
+# delete exited containers
+docker rm -v $(docker ps -a -q -f status=exited)
+# delete images
+docker rmi $(docker images -f "dangling=true" -q)
+# delete vfs
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm martin/docker-cleanup-volumes
